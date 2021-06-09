dde87468ad1fbdc416b177cacea4289aa06a1292
Victor Badenas <victor.badenas@gmail.com>
2021-06-09 19:06:16 +0200

    added unittesting for profile

--- test/test_user_input.py
+++ test/test_user_input.py
@@ -10,17 +10,17 @@ from constraints import Constraints
 
 class TestUserRequest(unittest.TestCase):
 
-    profile_str = "2, 1, Programming, 1, 3, 1, 0, 0, 0, 1, 0, 0"
+    profile_str = "2, 1, 0, 1, 3, 1, 0, 0, 0, 1, 0, 0"
     preferences_str = "5, 2, 3, 1, 2, 1, 3, 4"
     constraints_str = "cpu_brand: Intel, gpu_brand: PreferNVIDIA, max_budget: 1000"
 
     def test_profile(self):
         result = UserRequest._process_profile(None, self.profile_str)
-        self.assertEqual(result, None)
+        self.assertTrue(np.isclose(result, np.array([2., 1., 0., 1., 3., 1., 0., 0., 0., 1., 0., 0.])).all())
 
     def test_preferences(self):
         result = UserRequest._process_preferences(None, self.preferences_str)
-        self.assertTrue((result == np.array([5, 2, 3, 1, 2, 1, 3, 4])).all())
+        self.assertTrue((result == np.array([1.0, 0.25, 0.5, 0.0, 0.25, 0.0, 0.5, 0.75])).all())
 
     def test_constraints(self):
         target = Constraints({'cpu_brand' : 'Intel', 'gpu_brand' : 'PreferNVIDIA', 'max_budget' : '1000'})
